aa69f76a04d8d88ab0cf8988dd521e08978661cf
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2024-10-23 11:49:55 +0530

    switch

---
+++ Javascript_Revision/Basics_theory_Revision/01_Basics_introduction.js
@@ -0,0 +1,22 @@
+// comments are the important part of code and javascript they make code more understandable and readable js do not e
+// Js do not exceute comments 
+// anything starts with (//) is a single line comment 
+// anything in between (/** ............ */) is a multiple line comment
+
+// Variables==>
+
+// variables are the containers for value stored in them ,When the varaible is delecalred a memeory location is allowed to the same 
+// To declare a variable, we use var, let, or const keywords.
+
+// RULES FOR DELCARING VARAIBLES IN JS 
+
+// A JavaScript variable name should not begin with a number.
+// A JavaScript variable name does not allow special characters except dollar sign and underscore.
+// A JavaScript variable name follows a camelCase convention.
+// A JavaScript variable name should not have space between words.
+
+
+// example --> 
+let x = 20 //indicates a variable value which can  be upadated or re assign its a block scope varaiable
+const pi=3.1423 // indicates a constant value which can not be upadated or re assign its a block scope varaiable
+var y=30 ////indicates a variable value which can  be upadated or re assign its a function scope varaiable (var was the oldest way to delerae the varaible in js )
