ee536c3150cb35a2ce3effa0f5f9d54c683b3315
Siddhant Gadakh <siddhant.gadakh@expleogroup.com>
2024-11-12 12:21:33 +0530

    newTestcase

--- Javascript_Revision/Basics_theory_Revision/01_Basics_introduction.js
+++ Javascript_Revision/Basics_theory_Revision/01_Basics_introduction.js
@@ -20,3 +20,5 @@
 let x = 20 //indicates a variable value which can  be upadated or re assign its a block scope varaiable
 const pi=3.1423 // indicates a constant value which can not be upadated or re assign its a block scope varaiable
 var y=30 ////indicates a variable value which can  be upadated or re assign its a function scope varaiable (var was the oldest way to delerae the varaible in js ).
+
+//testting
